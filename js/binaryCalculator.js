8e15a2748922919f9f33f8ad0e121e25d2309e7b
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-02 16:13:27 +0100

    fix variables names to better representation of code

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -7,36 +7,39 @@ const addDivisionOperator = () => showToDisplay("/");
 
 const getDisplayElement = () => document.getElementById("res");
 
-const addToDisplay = operator => getDisplayElement().innerText = `${getDisplayElement().innerText}${operator}`;
+const addToDisplay =  => getDisplayElement().innerText = `${getDisplayElement().innerText}${operator}`;
 
 const showToDisplay = operator => {
     const matchOperator = getDisplayElement().innerHTML.match(/[01]*([\+-\/\*])[01]*/);
 
-    if (matchOperator !== null) getDisplayElement().innerText = getDisplayElement().innerHTML.replace(matchOperator[1], operator);
-    else addToDisplay(operator);
+    if (matchOperator !== null) {
+        getDisplayElement().innerText = getDisplayElement().innerHTML.replace(matchOperator[1], operator);
+    } else {
+        addToDisplay(operator);
+    }
 }
 
 const clearDisplay = () => getDisplayElement().innerText = "";
 
 const calculate = () => {
     const expression = getDisplayElement().innerHTML.match(/([10]+)([\+-\/\*])([10]+)/);
-    const operand1 = parseInt(expression[1], 2);
+    const firstOperand = parseInt(expression[1], 2);
     const operator = expression[2];
-    const operand2 = parseInt(expression[3], 2);
+    const secondOperand = parseInt(expression[3], 2);
     let result = 0;
 
     switch (operator) {
         case "+":
-            result = (operand1 + operand2).toString(2);
+            result = (firstOperand + secondOperand).toString(2);
             break;
         case "-":
-            result = (operand1 - operand2).toString(2);
+            result = (firstOperand - secondOperand).toString(2);
             break;
         case "*":
-            result = (operand1 * operand2).toString(2);
+            result = (firstOperand * secondOperand).toString(2);
             break;
         case "/":
-            result = (operand1 / operand2).toString(2);
+            result = (firstOperand / secondOperand).toString(2);
             break;
     }
 
