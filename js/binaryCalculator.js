48f9e30cff9537ca4bf8c15fb6eda42035ad7343
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-03 12:07:58 +0100

    fix variables names to better represent the code

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -1,40 +1,45 @@
 const addZero = () => addToDisplay("0");
 const addOne = () => addToDisplay("1");
-const addSumOperator = () => showToDisplay("+");
-const addSubtrationOperator = () => showToDisplay("-");
-const addMultiplicationOperator = () => showToDisplay("*");
-const addDivisionOperator = () => showToDisplay("/");
+const addSumOperator = () => setOperator("+");
+const addSubtrationOperator = () => setOperator("-");
+const addMultiplicationOperator = () => setOperator("*");
+const addDivisionOperator = () => setOperator("/");
 
 const getDisplayElement = () => document.getElementById("res");
 
-const addToDisplay = character => {
-    if (getDisplayElement().innerHTML === "You need to add a binary number first") {
-        clearDisplay();
-        getDisplayElement().innerText = `${getDisplayElement().innerText}${character}`;
-    } else {
-        getDisplayElement().innerText = `${getDisplayElement().innerText}${character}`;
-    }
-}
+const currentDisplayContent = () => getDisplayElement().innerHTML;
 
-const showToDisplay = operator => {
-    const matchOperator = getDisplayElement().innerHTML.match(/[01]*([\+-\/\*])[01]*/);
-
-    if (matchOperator !== null) {
-        getDisplayElement().innerText = getDisplayElement().innerHTML.replace(matchOperator[1], ` ${operator} `);
-    } else if (getDisplayElement().innerHTML !== "" && getDisplayElement().innerHTML !== "You need to add a binary number first") {
-        addToDisplay(` ${operator} `);
-    } else {
-        getDisplayElement().innerHTML = "You need to add a binary number first";
-    }
-}
+const setDisplayContent = content => getDisplayElement().innerText = content;
 
 const clearDisplay = () => getDisplayElement().innerText = "";
 
+const hasFirstBinaryOperand = () => getDisplayElement().innerHTML !== "" && getDisplayElement().innerHTML !== "You need to add a binary number first";
+
+const addToDisplay = character => {
+    if (currentDisplayContent() === "You need to add a binary number first") {
+        clearDisplay();
+    }
+    setDisplayContent(`${currentDisplayContent()}${character}`);
+}
+
+const setOperator = operator => {
+    const matchOperator = getDisplayElement().innerHTML.match(/[01]*([\+-\/\*])[01]*/);
+
+    if (matchOperator !== null) {
+        const currentOperator = matchOperator[1];
+        return setDisplayContent(currentDisplayContent().replace(currentOperator, `${operator}`));
+    }
+    if (hasFirstBinaryOperand()) {
+        return addToDisplay(`${operator}`);
+    }
+    setDisplayContent("You need to add a binary number first");
+}
+
 const calculate = () => {
-    const expression = getDisplayElement().innerHTML.match(/([10]+)([\+-\/\*])([10]+)/);
-    const firstOperand = parseInt(expression[1], 2);
-    const operator = expression[2];
-    const secondOperand = parseInt(expression[3], 2);
+    const binaryExpression = getDisplayElement().innerHTML.match(/([10]+)([\+-\/\*])([10]+)/);
+    const firstOperand = parseInt(binaryExpression[1], 2);
+    const operator = binaryExpression[2];
+    const secondOperand = parseInt(binaryExpression[3], 2);
     let result = 0;
 
     switch (operator) {
@@ -51,6 +56,5 @@ const calculate = () => {
             result = (firstOperand / secondOperand).toString(2);
             break;
     }
-
-    getDisplayElement().innerText = `${result}`;
+    setDisplayContent(`${result}`);
 }
